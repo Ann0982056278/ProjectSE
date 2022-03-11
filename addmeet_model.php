fe1406f86acf6c95a4d5a506e00b296a01e19ad0
Archann <chanika.k@kkumail.com>
2022-03-11 11:10:55 +0700

    comment

---
+++ addmeet_model.php
@@ -0,0 +1,30 @@
+<?php
+include('conn.php');
+
+$title = $_POST['title'];
+$head = $_POST['head'];
+$numattend = $_POST['numattend'];
+$listname = $_POST['listname'];
+$roomid = $_POST['roomid'];
+$start = $_POST['start'];
+$end = $_POST['end'];
+$addequipment = $_POST['addequipment'];
+$remark = $_POST['remark'];
+$meetfile = $_POST['meetfile'];
+$userid = $_POST['userid'];
+
+$file = $_FILES['meetfile'];
+$filename = $_FILES["meetfile"]["name"];
+$filTmpename = $_FILES["meetfile"]["tmp_name"];
+$fileExt = explode(".", $filename);
+$fileAcExt = strtolower(end($fileExt));
+$newFilename = time() . "." . $fileAcExt;
+$fileDes = 'upload/' . $newFilename;
+move_uploaded_file($filTmpename, $fileDes);
+$meetfilelocation = $fileDes;
+$meetfile = $meetfilelocation;
+
+mysqli_query($conn, "insert into 
+	meeting (title, head, numattend, listname, roomid, start, end, addequipment, userid, remark, meetfile) 
+	values ('$title', '$head', '$numattend', '$listname', '$roomid', '$start', '$end', '$addequipment', '$userid', '$remark', '$meetfile')");
+header('location:addmeet.php');
